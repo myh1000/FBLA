0e42d7f4845513c794e641afa5fcae49311a0353
myh1000 <myh1000@gmail.com>
2016-09-02 17:44:59 -0700

    css and 404 page

--- app.js
+++ app.js
@@ -63,6 +63,9 @@ if (app.get('env') === 'development') {
     });
   });
 }
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status).render(err.status, {title: "Sorry, page not found"});
 });
